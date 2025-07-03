6776049d3b663c3bd664f1af8c0508144969843c
Kizu17 <rogerraycruz17@gmail.com>
2025-07-03 18:13:15 +0800

    route fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   base: '/UpdatedPortfolio/',
-  plugins: [
-    react(), tailwindcss(),
-  ],
+  plugins: [react()],
 })
