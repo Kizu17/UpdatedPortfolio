1be8bbb23ac9af1f3f46566e252e275601d99877
Kizu17 <rogerraycruz17@gmail.com>
2025-07-03 18:18:22 +0800

    Fix nested Router issue and update config

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Internship from './pages/Internship'
 import Projects from './pages/Projects'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 function App() {
-useEffect(() => {
-  AOS.init({
-    duration: 1000, // animation duration
-    once: false,     // whether animation should happen only once
-  });
-}, []);
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: false })
+  }, [])
+
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/internship" element={<Internship />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/internship" element={<Internship />} />
+      <Route path="/projects" element={<Projects />} />
+    </Routes>
   )
 }
 
